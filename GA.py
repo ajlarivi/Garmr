8ee9cf6cd6046c108cfa64afaf4221251f4381fa
ajlarivi <andru.lariviere@gmail.com>
2019-07-19 19:19:53 -0700

    initial implementation of crossover function, not yet tested

--- GA.py
+++ GA.py
@@ -13,7 +13,7 @@ class Room:
 	def __init__(self, id, size):
 		self.id = id
 		self.size = size #capacity of the room
-		self.times = [] #array of time slots, each will contain a lecture object
+		self.times = [None]*35 #array of time slots, each will contain a lecture object
 
 def main():
 	fileString = sys.argv[1]
@@ -42,6 +42,8 @@ def main():
 	for room in rooms:
 		print(str(room.id) + "," + str(room.size))
 
+	
+
 
 
 
@@ -56,13 +58,32 @@ def main():
 def crossover(parent1, parent2):
 	#in crossover, iterate through each room, select classes from each parent
 
+	child = []
+
+	for i in range(len(parent1)):
+		childTimes = [None]*35
+
+		for j in range(len(parent1[i].times)):
+			if parent1[i].times[j] is not None and parent2[i].times[j] is None:
+				childTimes[j] = parent1[i].times[j]
+
+		for k in range(len(parent2[i].times)):
+			if parent1[i].times[k] is None and parent2[i].times[k] is not None:
+				childTimes[j] = parent2[i].times[k]
+
+		childRoom = Room(parent1.id,parent1.size)
+		childRoom.times = childTimes
+
+		child.append(childRoom)
+
+		print(child)
 	return child
 
 #returns a fitness value, the closer to zero the more fit the gene
 def fitnessFunction(chromosome): 
 	fitness = 0
 	fitness = fitness + sameRoomeSameTime(chromosome) #adds to the fitness value for classes scheduled in the same room at the same time
-	fitness = fitness + classCapacityExceeded(chromosome) #adds to the fitness value if a class is scheduled in a room that cant hold it
+	fitness = fitnesss + classCapacityExceeded(chromosome) #adds to the fitness value if a class is scheduled in a room that cant hold it
 	fitness = fitness + hoursAccurate(chromosome) #adds to the fitness value if a lecture has too many or too few in a week
 	fitness = fitness + repeatProf(chromosome) #(soft) adds to the fitness value for profs teaching in the same room two slots in a row
 	fitness = fitness + slotsOnSameDay(chromosome) #(soft) adds to the fitness value for classes being schedules more than once per day
@@ -72,15 +93,7 @@ def sameRoomeSameTime(chromosome):
 	pass
 
 def classCapacityExceeded(chromosome):
-	int i = 0
-	int j = 0
-		
-	for i in range(len[chromsome]):
-		for j in range(len[chromsome[i].times]):
-			chromosome[i].size < chromosome[i].times[j].size
-			return 100000
-			
-	return 0	
+	pass
 
 def hoursAccurate(chromosome):
 	pass
